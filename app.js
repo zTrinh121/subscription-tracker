6d6ae2934a0b43acf502bde3a2d4cb025f0b7d24
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2025-05-06 21:46:48 +0700

    Config files env

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { PORT } from "./config/env.js";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
---
+++ config/env.js
@@ -0,0 +1,7 @@
+import { config} from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('PORT:', process.env.PORT);
+
+export const { PORT, NODE_ENV } = process.env;
