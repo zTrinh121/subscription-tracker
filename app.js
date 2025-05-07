ca521cd2fbe06a59a4324528be0c0984f0acbff6
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2025-05-07 20:51:06 +0700

    Adding error handler, controllers and authentication (mongodb)

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 import express from 'express';
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 
 import userRoutes from "./routes/user.routes.js";
 import subscriptionRoutes from "./routes/subscription.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import connectToDatabase from "./mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscription', subscriptionRoutes);
 
+app.use(errorMiddleware);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
+
+    await connectToDatabase();
 })
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,10 @@
 import {Router} from "express";
+import {signIn, signOut, signUp} from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-
-authRouter.post("/sign-up", (req, res) => {
-    res.send({title: 'Sign Up'});
-})
-
-authRouter.post("/sign-in", (req, res) => {
-    res.send({title: 'Sign In'});
-})
-
-authRouter.post("/sign-out", (req, res) => {
-    res.send({title: 'Sign Out'});
-})
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn)
+authRouter.post("/sign-out", signOut)
 
 export default authRouter;
